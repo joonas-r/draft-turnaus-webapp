a6441e1c18536d391cba670540e3e898d764aa78
brothagenie <kie.mattila@gmail.com>
2026-04-30 14:22:38 +0300

    Update hamburger.js

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -1,3 +1,13 @@
-document.querySelector('.hamburger').addEventListener('click', function() {
-  document.querySelector('.menu').classList.toggle('open');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const icon = document.getElementById('hamburger-icon');
+
+hamburger.addEventListener('click', function() {
+    menu.classList.toggle('open');
+    
+    if (menu.classList.contains('open')) {
+        icon.src = '/icons/close.png';
+    } else {
+        icon.src = '/icons/hamburger.png';
+    }
 });
